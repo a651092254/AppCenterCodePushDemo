500cadb6a6084090c90b7528399310fc54b008c6
杨俊宁 <youngjuning@aliyun.com>
2020-02-26 21:49:54 +0800

    syncImmediate

--- CodePushUtils.js
+++ CodePushUtils.js
@@ -80,14 +80,22 @@ const syncImmediate = async () => {
   codePush.sync(
     {
       updateDialog: {
+        // 是否显示更新描述
         appendReleaseDescription: true,
-        title: '更新',
+        // 更新描述的前缀。 默认为"Description"
         descriptionPrefix: '\n\n更新内容：\n',
-        mandatoryContinueButtonLabel: '更新',
-        mandatoryUpdateMessage: '',
-        optionalIgnoreButtonLabel: '忽略',
-        optionalInstallButtonLabel: '更新',
-        optionalUpdateMessage: '有一个可用的更新，你要下载它吗？',
+        // 强制更新按钮文字，默认为continue
+        mandatoryContinueButtonLabel: '立即更新',
+        // 强制更新时的信息. 默认为"An update is available that must be installed."
+        mandatoryUpdateMessage: '必须更新后才能使用',
+        // 非强制更新时，按钮文字,默认为"ignore"
+        optionalIgnoreButtonLabel: '稍后',
+        // 非强制更新时，确认按钮文字. 默认为"Install"
+        optionalInstallButtonLabel: '后台更新',
+        // 非强制更新时，检查到更新的消息文本
+        optionalUpdateMessage: '有新版本了，是否更新？',
+        // Alert窗口的标题
+        title: '更新',
       },
       deploymentKey,
       installMode: codePush.InstallMode.IMMEDIATE,
